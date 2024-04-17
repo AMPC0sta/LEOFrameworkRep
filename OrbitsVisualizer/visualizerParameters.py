0dc906d5d8e3805d4d07ee0f36b554184172d4f4
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-04-17 22:42:17 +0100

    Adding earth tilt

--- OrbitsVisualizer/visualizerParameters.py
+++ OrbitsVisualizer/visualizerParameters.py
@@ -1,10 +1,17 @@
+from numpy import *
 
 class VisualizerParameters:
     
     # Constructor
-    def __init__(self,e_radius):
+    def __init__(self,e_radius,e_tilt):
+        
         if e_radius == None:
            self.e_radius = 6371 # Default Earth radius in km's
         else:
             self.e_radius = e_radius
+        
+        if e_tilt == None:
+           self.e_tilt = (23 + 23/60) *  pi/180   # Default Earth axis tilt  (Z), 23 degrees plus 23 minutes of the arc converted into rads
+        else:
+            self.e_tilt = e_tilt
             
