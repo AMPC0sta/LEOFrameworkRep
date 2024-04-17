897a3fb91781f4099e189a418fa15797c135739d
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-04-17 22:51:54 +0100

    adding e_center

--- OrbitsVisualizer/visualizerParameters.py
+++ OrbitsVisualizer/visualizerParameters.py
@@ -1,9 +1,10 @@
 from numpy import *
+from vpython import *
 
 class VisualizerParameters:
     
     # Constructor
-    def __init__(self,e_radius,e_tilt):
+    def __init__(self,e_radius,e_tilt,e_center):
         
         if e_radius == None:
            self.e_radius = 6371 # Default Earth radius in km's
@@ -14,4 +15,9 @@ class VisualizerParameters:
            self.e_tilt = (23 + 23/60) *  pi/180   # Default Earth axis tilt  (Z), 23 degrees plus 23 minutes of the arc converted into rads
         else:
             self.e_tilt = e_tilt
+            
+        if e_center == None:
+            self.e_center = vector(0,0,0)
+        else
+            self.e_center = e_center
             
