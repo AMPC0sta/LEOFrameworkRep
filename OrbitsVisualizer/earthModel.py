0814a55612bffb77bb1c6fed6d92d3752ac6d8ac
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-04-26 23:55:00 +0100

    adding EarthModel class

---
+++ OrbitsVisualizer/earthModel.py
@@ -0,0 +1,13 @@
+from vpython import *
+
+class EarthModel:
+ 
+    def __init__(self,earth_center=vector(0,0,0),radius=6371,canvas=canvas()):
+        self.earth_center=earth_center
+        self.radius=radius
+        self.canvas = canvas
+        
+        self.earth=sphere(canvas=self.canvas,pos=self.earth_center,radius=self.radius,texture=textures.earth,shininess=0)
+        self.canvas.lights=[]
+        self.canvas.lights.append(distant_light(direction=vector(-1,0,0)))
+        self.canvas.ambient=color.white*0.5
--- OrbitsVisualizer/startVisualizer.py
+++ OrbitsVisualizer/startVisualizer.py
@@ -2,7 +2,7 @@ from vpython import *
 from numpy import *
 from visualizerParameters import VisualizerParameters
 from coordinateSystem import *
-
+from earthModel import EarthModel
 
 width=1320
 height=650
@@ -13,8 +13,9 @@ screen = canvas(width=(width*4/5)-50,height=height,align='left',title=main_title
 l = parameters.e_radius
 print(l)
 c = CoordinateSystem(lenght=l)
-print(c.angle)
 c.transform_from_vpython_to_ecef()
+
+earth = EarthModel(canvas=screen,radius=l)
 
 while True:
     pass
