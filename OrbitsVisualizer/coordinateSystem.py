0f5085d3f7feb59f6a13ba7458a7132f9c523d63
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-04-28 00:22:40 +0100

    Adding checkbox to hide coordinates axis

--- OrbitsVisualizer/coordinateSystem.py
+++ OrbitsVisualizer/coordinateSystem.py
@@ -7,12 +7,13 @@ class CoordinateSystem:
     z_axis_color = color.yellow
     axis_shaft = 50
     
-    def __init__(self,type='ecef',center=vector(0,0,0),lenght=1,angle=0,visible=True):
+    def __init__(self,type='ecef',center=vector(0,0,0),lenght=1,angle=0,visible=True,screen=canvas()):
    
         self.type = type
         self.center = center
         self.lenght = lenght *1.1
         self.angle = angle * pi/180
+        self.screen = screen
         
         self.x_vector = 1.5 * vector(self.lenght,0,0)
         self.y_vector = 1.5 * vector(0,self.lenght,0)
@@ -22,14 +23,14 @@ class CoordinateSystem:
         self.rot_axis_y = vector(0,1,0)
         self.rot_axis_z = vector(0,0,1)
         
-        self.x_axis = arrow(center=self.center,axis=self.x_vector,color=CoordinateSystem.x_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True,visible=True)
-        self.x_lbl = label(pos=self.x_vector,text='X',color=CoordinateSystem.x_axis_color,box=False)
+        self.x_axis = arrow(canvas=self.screen,center=self.center,axis=self.x_vector,color=CoordinateSystem.x_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True,visible=True)
+        self.x_lbl = label(canvas=self.screen,pos=self.x_vector,text='X',color=CoordinateSystem.x_axis_color,box=False)
 
-        self.y_axis = arrow(center=self.center,axis=self.y_vector,color=CoordinateSystem.y_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True)
-        self.y_lbl = label(pos=self.y_vector,text='Y',color=CoordinateSystem.y_axis_color,box=False)
+        self.y_axis = arrow(canvas=self.screen,center=self.center,axis=self.y_vector,color=CoordinateSystem.y_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True)
+        self.y_lbl = label(canvas=self.screen,pos=self.y_vector,text='Y',color=CoordinateSystem.y_axis_color,box=False)
 
-        self.z_axis = arrow(center=self.center,axis=self.z_vector,color=CoordinateSystem.z_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True)
-        self.z_lbl = label(pos=self.z_vector,text='Z',color=CoordinateSystem.z_axis_color,box=False)
+        self.z_axis = arrow(canvas=self.screen,center=self.center,axis=self.z_vector,color=CoordinateSystem.z_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True)
+        self.z_lbl = label(canvas=self.screen,pos=self.z_vector,text='Z',color=CoordinateSystem.z_axis_color,box=False)
            
         def get_space_axis(): 
             return (self.x_axis, self.y_axis, self.z_axis)
@@ -38,6 +39,17 @@ class CoordinateSystem:
         def get_space_axis_labels():
             return (self.x_lbl,self.y_lbl,self.z_lbl)
         
+    def visibility(self,state):
+        self.state = state
+        self.x_axis.visible = self.state
+        self.x_lbl.visible = self.state
+        
+        self.y_axis.visible = self.state
+        self.y_lbl.visible = self.state
+
+        self.z_axis.visible = self.state
+        self.z_lbl.visible = self.state
+
         
     def transform_from_vpython_to_ecef(self):
         self.angle = pi/2
--- OrbitsVisualizer/startVisualizer.py
+++ OrbitsVisualizer/startVisualizer.py
@@ -8,14 +8,22 @@ width=1320
 height=650
 main_title = 'Orbit Visualizer'
 
-parameters = VisualizerParameters()
 screen = canvas(width=(width*4/5)-50,height=height,align='left',title=main_title)
+parameters = VisualizerParameters()
 l = parameters.e_radius
-print(l)
-c = CoordinateSystem(lenght=l)
-c.transform_from_vpython_to_ecef()
+c = CoordinateSystem(lenght=l,screen=screen)
 
+def manage_axises(event):
+    if event.checked:
+        c.visibility(True)
+    else:
+        c.visibility(False)
+        
+
+c.transform_from_vpython_to_ecef()
 earth = EarthModel(canvas=screen,radius=l)
+
+chk_axises = checkbox(text='Show X,Y,Z Axis',bind=manage_axises,pos=screen.caption_anchor,checked=True)
 
 while True:
     pass
