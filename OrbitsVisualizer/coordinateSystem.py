9a29149d96da9089a1ae5c95ca95eba68408634e
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-04-29 01:09:42 +0100

    adding comments

--- OrbitsVisualizer/coordinateSystem.py
+++ OrbitsVisualizer/coordinateSystem.py
@@ -2,27 +2,33 @@ from vpython import *
 
 class CoordinateSystem:
 
+
+    # axis colors and global definitions setup
     x_axis_color = color.blue
     y_axis_color = color.red
     z_axis_color = color.yellow
     axis_shaft = 50
     
+    
     def __init__(self,type='ecef',center=vector(0,0,0),lenght=1,angle=0,visible=True,screen=canvas()):
-   
+        # Internal state
         self.type = type
         self.center = center
         self.lenght = lenght *1.1
         self.angle = angle * pi/180
         self.screen = screen
         
+        # Max lenght in vectorial way
         self.x_vector = 1.3 * vector(self.lenght,0,0)
         self.y_vector = 1.3 * vector(0,self.lenght,0)
         self.z_vector = 1.3 * vector(0,0,self.lenght)
         
+        # Norm vectors (to act as rotation pivot's)
         self.rot_axis_x = vector(1,0,0)
         self.rot_axis_y = vector(0,1,0)
         self.rot_axis_z = vector(0,0,1)
         
+        # Drawing system
         self.x_axis = arrow(canvas=self.screen,center=self.center,axis=self.x_vector,color=CoordinateSystem.x_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True,visible=True)
         self.x_lbl = label(canvas=self.screen,pos=self.x_vector,text='X',color=CoordinateSystem.x_axis_color,box=False)
 
@@ -31,6 +37,7 @@ class CoordinateSystem:
 
         self.z_axis = arrow(canvas=self.screen,center=self.center,axis=self.z_vector,color=CoordinateSystem.z_axis_color,shaftwidth=CoordinateSystem.axis_shaft,round=True)
         self.z_lbl = label(canvas=self.screen,pos=self.z_vector,text='Z',color=CoordinateSystem.z_axis_color,box=False)
+          
            
         def get_space_axis(): 
             return (self.x_axis, self.y_axis, self.z_axis)
@@ -38,6 +45,7 @@ class CoordinateSystem:
         
         def get_space_axis_labels():
             return (self.x_lbl,self.y_lbl,self.z_lbl)
+        
         
     def visibility(self,state):
         self.state = state
@@ -55,6 +63,7 @@ class CoordinateSystem:
         return self.state
     
         
+    # VPython axis order are Y,Z,X instead of X,Y,Z so that a transformation is needed to the axis    
     def transform_from_vpython_to_ecef(self):
         self.angle = pi/2
 
