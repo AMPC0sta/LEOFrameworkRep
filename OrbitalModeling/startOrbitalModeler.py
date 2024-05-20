ab4048fd1ce68bb08fbed0437fc41af9e8953505
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-05-20 19:20:22 +0100

    split actions, on missions phases, one behavior for one click and another for double click

--- OrbitalModeling/startOrbitalModeler.py
+++ OrbitalModeling/startOrbitalModeler.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter.font import Font
+from  tkcalendar import *
 
 button_ptr=0
 operations = []
@@ -8,8 +9,15 @@ operations = []
 screen = Tk(className=' Low Earth Orbit Calculator')
 screen.geometry('1600x800')
 
-def trigger_op_button(ptr):
+def trigger_op_button_one_click(event):
+    pass
+
+def trigger_op_button_double_click(event):
+    global operations
+    
     open_input_operation_parameters_form()
+
+
 
 
 def insert_mission_ops_button():
@@ -18,22 +26,50 @@ def insert_mission_ops_button():
     
     bold_font = Font(family="Helvetica", size=10, weight="bold")
     
-    operations.append(Button(frame_top, text=str(button_ptr+1)+':Click to Edit Phase',command=lambda: trigger_op_button(button_ptr),font=bold_font))
+    #operations.append(Button(frame_top, text=str(button_ptr+1)+':Click to Edit Phase',command=lambda: trigger_op_button(button_ptr),font=bold_font))
+    operations.append(Button(frame_top, text=str(button_ptr+1)+':Double click to Edit Phase',font=bold_font))
     operations[button_ptr].pack(side=LEFT, padx=2, pady=5, expand=True, fill=X)
+    operations[button_ptr].bind('<Button-1>',trigger_op_button_one_click)
+    operations[button_ptr].bind('<Double-1>',trigger_op_button_double_click)
 
     button_ptr=button_ptr+1
     
     
 def open_input_operation_parameters_form():    
+    
+    def select_tle_file():
+        file_path = filedialog.askopenfilename(title="Select a TLE file", filetypes=(("TLE files", "*.tle"), ("All files", "*.*")))
+        if file_path:
+            tle_entry.delete(0, END)
+            tle_entry.insert(0, file_path)
+            tle_entry.focus_set()
+        
+    
     popup = Toplevel(screen)
     popup.geometry('500x350')
     popup.title("Setup Mission Phase")
     
-    Label(popup, text="Phase Name:").pack()
-    Entry(popup).pack()
+    Label(popup, text="Instance Name:").grid(row=0, column=0, padx=5, pady=5, sticky="e")
+    name_entry = Entry(popup)
+    name_entry.grid(row=0, column=1, padx=5, pady=5)
     
-    Button(popup, text="OK").pack()
+    Label(popup, text="TLE File:").grid(row=1, column=0, padx=5, pady=5, sticky="e")
+    tle_entry = Entry(popup)
+    tle_entry.grid(row=1, column=1, padx=5, pady=5)
+    tle_button = Button(popup, text="Browse", command=select_tle_file)
+    tle_button.grid(row=1, column=2, padx=5, pady=5)
 
+    Label(popup, text="Start Datetime:").grid(row=2, column=0, padx=5, pady=5, sticky="e")
+    start_date_entry = DateEntry(popup, selectmode='day', date_pattern='yyyy-mm-dd')
+    start_date_entry.grid(row=2, column=1, padx=5, pady=5)
+
+    Label(popup, text="End Datetime:").grid(row=3, column=0, padx=5, pady=5, sticky="e")
+    end_date_entry = DateEntry(popup, selectmode='day', date_pattern='yyyy-mm-dd')
+    end_date_entry.grid(row=3, column=1, padx=5, pady=5)
+
+    # Create an OK button to submit the input
+    ok_button = Button(popup, text="OK", command=get_input)
+    ok_button.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
 
 # to be used in event management (undefined events)
 def do_nothing():
