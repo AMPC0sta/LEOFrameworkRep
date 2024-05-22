f1c4d6fa7a1f7bee65d91cd08a18b90c1a0a67df
ANTONIO.A.COSTA <antonio.a.costa@oracle.com>
2024-05-22 15:56:23 +0100

    loadind TLE file to phase parameters class

--- OrbitalModeling/missionPhaseParameters.py
+++ OrbitalModeling/missionPhaseParameters.py
@@ -1,8 +1,21 @@
+from pyorbital.orbital import Orbital
 
 class MissionPhaseParameters:
     
-    def __init__(self,phase_name,tle_file):
+    def __init__(self,phase_name,tle_file = None):
         self.phase_name = phase_name
         self.tle_file = tle_file
         
-        
+    
+    def read_object_name(self):
+        if self.tle_file != None:
+            f = open(self.tle_file,'r')
+            self.object_name = f.readline()
+            f.close()
+        else:
+            raise RuntimeError("There's no TLE file for this mission phase!")
+
+
+    def load_TLE_data(self):
+        self.orbital_data = Orbital(self.object_name,self.tle_file)
+
