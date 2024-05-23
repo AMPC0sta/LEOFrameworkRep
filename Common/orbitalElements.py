13ae2ee7a34ccf35a04d04505b80531711d3572a
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-05-24 00:43:32 +0100

    adding period to orbital elements and correcting angular units

--- Common/orbitalElements.py
+++ Common/orbitalElements.py
@@ -14,11 +14,12 @@ class OrbitalElements:
         mean_motion=oe.mean_motion
         return (
             f"Time (UTC): {utc_time.isoformat()}\n"
-            f"Mean Anomaly: {oe.mean_anomaly} º\n"
+            f"Mean Anomaly: {oe.mean_anomaly} rads\n"
             f"Mean Motion: {mean_motion} revolutions per day\n"
             f"Semi Major Axis: {oe.semi_major_axis} km\n"
-            f"Argument of Perigee: {oe.arg_perigee} º\n"
+            f"Argument of Perigee: {oe.arg_perigee} rads\n"
             f"Eccentricity: {oe.excentricity}\n"
-            f"Inclination: {oe.inclination} º\n"
-            f"RAAN (Right Ascension of Ascending Node): {oe.right_ascension} º\n"
+            f"Inclination: {oe.inclination} rads\n"
+            f"RAAN (Right Ascension of Ascending Node): {oe.right_ascension} rads\n"
+            f"Period {oe.period} minutes\n"
         )
