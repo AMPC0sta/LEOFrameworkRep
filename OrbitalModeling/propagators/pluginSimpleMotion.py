9d5b6afb5758299c74c83e50c50ceebffc0fec7f
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-07-03 00:32:18 +0100

    creating and adding a new plugin for testing

---
+++ OrbitalModeling/propagators/pluginSimpleMotion.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta
+
+from propagatorSuperClass import PropagatorSuperClass
+
+  
+class PluginSimpleMotion(PropagatorSuperClass):
+     
+    def __init__(self):
+        super()
+        self.plugin_name = 'Simple'  
+        
+
+    def name_to_register_plugin(self):
+    
+        return self.plugin_name
+    
+
+    def set_startime(self,startime):
+        pass
+    
+    def test():
+        pass
+    
+    
