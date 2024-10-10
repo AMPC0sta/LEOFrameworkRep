b55c00cacbec3533b482042c5c824d5345fd2edd
AMPC0sta <75615924+AMPC0sta@users.noreply.github.com>
2024-10-10 14:44:05 +0100

    adding rotX matrix

--- OrbitsVisualizer/inertialReferences.py
+++ OrbitsVisualizer/inertialReferences.py
@@ -1,8 +1,19 @@
 
+import numpy as np
+
 class IntertialReferences:
-   
-   
+
         def __init__(self):
             pass
         
-        
+            
+        def rotX(theta_rad):
+            
+            rotational_X_matrix = np.array(
+                                            [
+                                                [1,     0,                 0                    ],
+                                                [0,     np.cos(theta_rad),  -np.sin(theta_rad)  ],
+                                                [0,     np.sin(theta_rad),  np.cos(theta_rad)   ]
+                                            ]    
+                                          )
+            return rotational_X_matrix
